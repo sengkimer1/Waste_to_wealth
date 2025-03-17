687201a93eb01abc2f30dfe77c3c4ae2cc24a37b
sengkimer1 <kimer.seng@institute.pse.ngo>
2025-03-17 16:13:51 +0700

    done login but need to change navigatoin more

--- lib/pages/splash.page.dart
+++ lib/pages/splash.page.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
+import 'login.page.dart';  // Import the next page
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Wait for 3 seconds and then navigate to the login page
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/splash.webp', width: 200),
-              const SizedBox(height: 10),
-              const Text(
-                'Waste to Wealth',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/splash.webp', width: 200),
+            const SizedBox(height: 10),
+            const Text(
+              'Waste to Wealth',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
